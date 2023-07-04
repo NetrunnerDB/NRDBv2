1f9677c60e3f9e579063c4f703040db25939a9b6
Ricardo Mendes <rokusu@gmail.com>
2023-07-04 23:09:52 +0100

    tweak icon component for green tests

--- app/components/icon.js
+++ app/components/icon.js
@@ -2,8 +2,6 @@ import Component from '@glimmer/component';
 
 export default class IconComponent extends Component {
   get id() {
-    if (this.args.id) {
-      return this.args.id.toLowerCase().replace(/[ _]/g, '-');
-    }
+    return this.args.id?.toLowerCase().replace(/[ _]/g, '-');
   }
 }
--- tests/integration/components/icon-test.js
+++ tests/integration/components/icon-test.js
@@ -6,21 +6,16 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | icon', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it adjusts the id', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Icon />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
     await render(hbs`
-      <Icon>
-        template block text
-      </Icon>
+      <Icon @id="HELLO WORLD" />
     `);
 
-    assert.dom(this.element).hasText('template block text');
+    let icon = this.element.querySelector('i');
+    assert.dom(icon).hasClass('icon-hello-world');
+    assert.dom(icon).hasClass('hello-world');
   });
 });
