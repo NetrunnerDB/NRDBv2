23d78d8838128011e42f2a09fb680c509c95a878
Ricardo Mendes <rokusu@gmail.com>
2023-07-05 17:25:15 +0100

    minor tweaks to banlists, factions and card display

--- app/routes/page/faction.js
+++ app/routes/page/faction.js
@@ -1,23 +1,22 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class PageFactionRoute extends Route {
   @service store;
 
   async model(params) {
     // Get the faction object
-    let faction = await this.store.findRecord('faction', params.id);
+    let faction = this.store.findRecord('faction', params.id);
 
     // Get all ids for the faction
     // TODO: sort ids by legality then alphabetically
     // TODO: add a way to the API to determine a card's legality in a format without pulling that format
-    let ids = await this.store.query('card', {
-      filter: { search: `t:identity f:${faction.id}` },
+    let ids = this.store.query('card', {
+      filter: { search: `t:identity f:${params.id}` },
+      include: 'printings',
     });
 
-    return {
-      faction: faction,
-      ids: ids,
-    };
+    return hash({ faction, ids });
   }
 }
