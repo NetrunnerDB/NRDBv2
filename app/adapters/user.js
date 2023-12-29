521394c3f04d94d38fe06e8b29aa1ad7bf84323e
Jason Gessner <jason@multiply.org>
2023-12-29 12:09:05 -0600

    Fix lint errors.

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -3,7 +3,7 @@ import ApplicationAdapter from './application';
 export default class UserAdapter extends ApplicationAdapter {
   namespace = 'v3/private';
   // The API endpoint is /api/v3/private/user and presents as a singleton.
-  pathForType(type) {
+  pathForType() {
     return 'user';
   }
 }
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,10 @@
-import Model, { attr, hasMany } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class UserModel extends Model {
-  query(store, type, query) {
-    return fetch('/user').then(function(users) { return users.length == 1 ? users[0] : {} });
+  query() {
+    return fetch('/user').then(function (users) {
+      return users.length == 1 ? users[0] : {};
+    });
   }
   @attr username;
 }
