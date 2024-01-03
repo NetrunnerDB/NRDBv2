1b2fd36cd6f817a48520c8fda1c627b77e8d3e07
Jason Gessner <jason@multiply.org>
2024-01-02 23:17:17 -0600

    Update user model and adapter.

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,9 +1,12 @@
 import ApplicationAdapter from './application';
+import ENV from 'netrunnerdb/config/environment';
 
 export default class UserAdapter extends ApplicationAdapter {
+  host = ENV.API_URL;
   namespace = 'v3/private';
+
   // The API endpoint is /api/v3/private/user and presents as a singleton.
-  pathForType() {
+  pathForType(type) {
     return 'user';
   }
 }
