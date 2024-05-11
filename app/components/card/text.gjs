516e1e5ccb6b78b647a1f3a625fa09415dd72daf
distributive <github@ameliewd.com>
2024-05-11 23:56:13 +0100

    More things

--- app/components/card/text.gjs
+++ app/components/card/text.gjs
@@ -2,7 +2,10 @@ import { htmlSafe } from '@ember/template';
 
 const process = (text) =>
   htmlSafe(
-    text.replaceAll('[trash]', '[trash-ability]').replaceAll('\n', '</p><p>'),
+    text
+      .replaceAll('[trash]', '[trash-ability]')
+      .replace(/\[([^\d]*?)\]/g, '<i class="icon-$1"></i>') // Convert icons
+      .replaceAll('\n', '</p><p>'),
   );
 
 <template>
--- app/utils/template-operators.js
+++ app/utils/template-operators.js
@@ -1,11 +1,14 @@
 export const and = (a, b) => a && b;
 export const or = (a, b) => a || b;
 export const eq = (a, b) => a === b;
-export const ne = (a, b) => a !== b;
+export const notEq = (a, b) => a !== b;
 export const div = (a, b) => a / b;
 export const mul = (a, b) => a * b;
 export const add = (a, b) => a + b;
 export const sub = (a, b) => a - b;
+export const not = (a) => !a;
+export const isEmpty = (a) => a === undefined || a === null;
+export const notEmpty = (a) => !isEmpty(a);
 export const maybe = (val, def = 'X') => {
   return val !== null && val != -1 ? val : def;
 };
