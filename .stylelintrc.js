02fa2130d2b65f3c918c0f048161853ad2796684
Ricardo Mendes <rokusu@gmail.com>
2024-11-05 18:03:38 +0000

    address style lints

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'custom-property-pattern': null,
+    'block-no-empty': null,
+  },
 };
