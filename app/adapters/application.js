125b7dccb49ac0548e8156c9fdfb6d3a92a8d26b
Ricardo Mendes <rokusu@gmail.com>
2025-05-16 23:28:51 +0100

    fix service inject deprecation

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,4 @@
-import { inject as service } from '@ember/service';
+import { service } from '@ember/service';
 import OIDCJSONAPIAdapter from 'ember-simple-auth-oidc/adapters/oidc-json-api-adapter';
 import { pluralize } from 'ember-inflector';
 import ENV from 'netrunnerdb/config/environment';
