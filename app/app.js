8439feab9d3708259cbd31cb1e931f34e0c1e3b2
Ricardo Mendes <rokusu@gmail.com>
2025-06-21 00:26:01 +0100

    run vite codemod

--- app/app.js
+++ app/app.js
@@ -1,13 +1,15 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'netrunnerdb/config/environment';
+import config from "./config/environment";
 import './font-awesome'; // Add this import statement for Font Awesome setup
+
+import compatModules from "@embroider/virtual/compat-modules";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
