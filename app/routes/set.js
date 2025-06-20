b608c621bfd164becec0be1af49b04c08f2fbf4f
Ricardo Mendes <rokusu@gmail.com>
2025-06-20 17:19:27 +0100

    normalize store identity names

--- app/routes/set.js
+++ app/routes/set.js
@@ -6,11 +6,11 @@ export default class SetsRoute extends Route {
   @service store;
 
   async model(params) {
-    let cardSet = this.store.findRecord('cardSet', params.id, {
+    let cardSet = this.store.findRecord('card-set', params.id, {
       include: ['printings', 'printings.card_type'],
     });
 
-    let allSets = await this.store.query('cardSet', {
+    let allSets = await this.store.query('card-set', {
       sort: 'date_release',
     });
     let setIndex = allSets.findIndex((set) => set.id == params.id);
