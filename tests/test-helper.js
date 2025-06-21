49badab5bcce638f5b0b9c1532fc086955037d71
Ricardo Mendes <rokusu@gmail.com>
2025-06-21 13:42:48 +0100

    upgrade ember-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,9 +4,12 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
-
+setupEmberOnerrorValidation();
+loadTests();
 start();
