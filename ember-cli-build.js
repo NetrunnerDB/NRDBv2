434b34f83f473bb88dc4c9a05e5368c55cead0ff
Ricardo Mendes <rokusu@gmail.com>
2025-08-13 03:51:51 +0100

    ember-vite-codemod

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,15 @@
-'use strict';
-
+'use strict';;
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const {
+  compatBuild
+} = require("@embroider/compat");
+
 module.exports = async function (defaults) {
+  const {
+    buildOnce
+  } = await import("@embroider/vite");
+
   const app = new EmberApp(defaults, {
     'ember-bootstrap': {
       insertEmberWormholeElementToDom: false,
@@ -22,5 +29,5 @@ module.exports = async function (defaults) {
     },
   });
 
-  return app.toTree();
+  return compatBuild(app, buildOnce);
 };
