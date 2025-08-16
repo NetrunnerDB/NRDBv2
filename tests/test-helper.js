7574dfb446d3b6bec892f058d4917ac65f36f16d
Ricardo Mendes <rokusu@gmail.com>
2025-08-16 16:53:00 +0100

    v5.12.0...v6.6.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,9 +3,8 @@ import config from 'netrunnerdb/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
 import { loadTests } from 'ember-qunit/test-loader';
-import { setupEmberOnerrorValidation } from 'ember-qunit';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 
